97ddc6baf3d1beaa110cc06c0badc4b47be58459
Andres Tovar <andres.tovar@king.com>
2020-06-20 11:43:55 +0200

    Board script

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+	private int[,] grid;
+
+	public int[,] Grid
+	{
+		get { return grid; }
+	}
+
+	private void Awake()
+	{
+		Initialise(5, 8);
+	}
+
+	public void Initialise(int rows, int columns)
+	{
+		grid = new int[rows, columns];
+
+		for (int r = 0; r < rows; r++)
+		{
+			for (int c = 0; c < columns; c++)
+			{
+				grid[r, c] = 2;
+			}
+		}
+	}
+}
